a00bd2cd695fae6c094477c3add289395512efe3
Prabhjeetsingh <prabhjeet8963@gmail.com>
2026-09-18 00:30:17 +0530

    Add Two Sum solution

---
+++ Leetcode_practice/twoSum.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        val_idx = {}  # key is value, value is index
+
+        for i, num in enumerate(nums):
+            if target - num in val_idx:
+                return [i, val_idx[target - num]]
+            
+            val_idx[num] = i
